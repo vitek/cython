9c6889b7c9b1f018eb2665a65e65ee79acdf5ad5
Vitja Makarov <vitja.makarov@gmail.com>
2011-02-20 20:01:47 +0300

    One more test case

--- tests/warnings/w_uninitialized.pyx
+++ tests/warnings/w_uninitialized.pyx
@@ -14,7 +14,18 @@ def simple_pos(arg):
         a = 0
     return a
 
+def ifelif(c1, c2):
+    if c1 == 1:
+        if c2:
+            a = 1
+        else:
+            a = 2
+    elif c1 == 2:
+        a = 3
+    return a
+
 _ERRORS = """
 2:11: Variable 'a' is used uninitialized
 8:12: Variable 'a' may be used uninitialized
+25:12: Variable 'a' may be used uninitialized
 """
