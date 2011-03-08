2aad73eaee10391a4d6eae6cae93afcc9d59be96
Vitja Makarov <vitja.makarov@gmail.com>
2011-03-08 21:26:15 +0300

    Some more test cases

--- tests/warnings/w_uninitialized_for.pyx
+++ tests/warnings/w_uninitialized_for.pyx
@@ -18,7 +18,19 @@ def simple_for_pos(n):
         a = 0
     return a
 
+def simple_target(n):
+    for i in n:
+        pass
+    return i
+
+def simple_target_f(n):
+    for i in n:
+        i *= i
+    return i
+
 _ERRORS = """
 6:12: local variable 'a' might be referenced before assignment
 12:12: local variable 'a' might be referenced before assignment
+24:12: local variable 'i' might be referenced before assignment
+29:12: local variable 'i' might be referenced before assignment
 """
